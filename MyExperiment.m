872fa61cee4b2827cf34f071e6be74de0485969e
Zhengguo Cheng <chengzhengguo123@qq.com>
2016-02-04 12:33:35 +0800

    image correction
    将偏斜的纱管矫正成垂直的。。。（这个版本会有判断问题，无法矫正向左偏斜的纱管）

--- MyExperiment.m
+++ MyExperiment.m
@@ -144,4 +144,56 @@ for c = 1:count
     figure;imshow(MatOfPic_Crop{c});
 end
 disp('已完成截图存图操作');
+disp('现在开始矫正图像');
+
+%将截出来的纱管进行旋转矫正，并垂直投影。
+%终止条件是，垂直投影的像素宽度达到最小
+angle = 1;  %定义纱管旋转的角度的每次分量数
+a = zeros(2,6); %用来存放每次校正图的投影有效宽度，以作比对
+
+for c = 1:count
+    flag = 0;   %循环终止条件
+    count = 0;  %计数求投影有效区域的宽度
+    while(flag ~= 1)
+        %================================================
+        %%%%第一次垂直投影
+        %================================================
+        X= sum(MatOfPic_Crop{c}); %第一次垂直投影
+        figure;plot(X);
+        [r1,l1] = size(X);
+        for b = 1:l1
+            if X(1,b) ~= 0              %计数求宽度
+                %  a(1,1) = a(1,1) + 1;
+                count = count + 1;
+            end
+        end
+        a(1,c) = count;
+        count = 0;
+        %==========================================================
+        MatOfPic_Crop{c} = imrotate(MatOfPic_Crop{c},angle);  %将图逆时针旋转angle个角度
+        %==========================================================
+        %%%%第二次垂直投影
+        %==========================================================
+        Y= sum(MatOfPic_Crop{c}); %第二次垂直投影
+        figure;plot(Y);
+        [r2,l2] = size(Y);
+        for b = 1:l2
+            if Y(1,b) ~= 0              %计数
+                %    a(2,1) = a(2,1) + 1;
+                count = count + 1;
+            end
+        end
+        a(2,c) = count;
+        %==========================================================
+        %判断
+        if a(2,1) >= a(1,1)
+            flag = 1;
+            MatOfPic_Crop{1} = imrotate(MatOfPic_Crop{1},-angle);
+        else
+            count = 0;
+        end        
+    end
+    fprintf('第%d根纱管图像矫正完成：）\n',c);
+    figure;imshow(MatOfPic_Crop{c});
+end
     
